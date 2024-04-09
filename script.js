1c2913d6a58f98a2d1f80e68be5171d264debb63
Akash Mailar <72142466+akash143143@users.noreply.github.com>
2024-04-09 15:50:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
-let btn1 = document.getElementById('button1');
-        let btn2 = document.getElementById('button2');
-        let copyDiv = document.querySelector('.copyCode');
-        let rgb1 = "#004773";
-        let rgb2 = "#54d542";
+let btn1 = document.querySelector('.btn1');
+        let btn2 = document.querySelector('.btn2');
+        let rectDiv = document.querySelector('.rectangle');
+        let rgb1 = "#34d6f4";
+        let rgb2 = "#296371";
 
-        
-        // 0 1 2 3 4 5 6 7 8 9 a b c d e f
+
         const hexaValues = () => {
-            let myHexaValues = "0123456789abcdef";
-            let colors = "#";
+            // '0 1 2 3 4 5 6 7 8 9 a b c d e f' == 16
+            let hexaValue = "0123456789abcdef";
+            let color = "#";
 
-           for(let i = 0; i < 6; i++){
-            colors = colors + myHexaValues[Math.floor(Math.random() * 16)];
-           }
-           return colors;
+            for(let i = 0; i < 6; i++){
+                color = color + hexaValue[Math.floor(Math.random() * 16)];
+            }
+
+            return color;
         }
 
-        const handleButton1 = () => {
-           rgb1 = hexaValues();
-        //    console.log(rgb1);
-           btn1.innerText = rgb1;
-           document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
-           copyDiv.innerHTML = `background-image: linear-gradient(to <br>right, ${rgb1}, ${rgb2});`;
+        const handleEvent1 = () => {
+            rgb1 = hexaValues();
+            // console.log(rgb1);
+            btn1.innerText = rgb1;
+            document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+            rectDiv.innerHTML = `background-image: linear-gradient(to right, ${rgb1}, ${rgb2});`;
         }
 
-        const handleButton2 = () => {
-           rgb2 = hexaValues();
-        //    console.log(rgb2);
-           btn2.innerText = rgb2;
-           document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
-           copyDiv.innerHTML = `background-image: linear-gradient(to <br>right, ${rgb1}, ${rgb2});`;
+        const handleEvent2 = () => {
+            rgb2 = hexaValues();
+            // console.log(rgb2);
+            btn2.innerText = rgb2;
+            document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+            rectDiv.innerHTML = `background-image: linear-gradient(to right, ${rgb1}, ${rgb2});`;
         }
-
-
-        btn1.addEventListener('click', handleButton1);
-        btn2.addEventListener('click', handleButton2);
         
-        copyDiv.addEventListener('click', () => {
-            navigator.clipboard.writeText(copyDiv.innerText);
-            alert("code copied!")
-        });
+
+        rectDiv.addEventListener('click', () => {
+            navigator.clipboard.writeText(rectDiv.innerText);
+
+            alert('code copied...');
+        })
+
+        
+        btn1.addEventListener('click', () => handleEvent1());
+        btn2.addEventListener('click', () => handleEvent2());
